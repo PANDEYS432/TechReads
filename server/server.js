d63e715400a8698beb0d9d47639d9bbdcfed02c0
PANDEYS432 <98703881+PANDEYS432@users.noreply.github.com>
2024-08-25 20:16:49 +0530

    some change
    some change

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
-const express= require('express');
-const app= express();
+const express = require('express');
+const app = express();
 app.use(express.json());
 require("dotenv").config();
 
 const dbconfig = require("./config/dbconfig.jsx");
-const port= process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
+
 const usersRoute = require("./routes/usersRoute.jsx");
 const booksRoute = require("./routes/booksRoute.jsx");
 const issuesRoute = require("./routes/issuesRoute.jsx");
 const reportsRoute = require("./routes/reportsRoute.jsx");
+
 app.use("/api/users", usersRoute);
 app.use("/api/books", booksRoute);
 app.use("/api/issues", issuesRoute);
